592a179350d991f6b3c8c172a8957ed3ff649e97
Lauris-21 <laurasanchez0301@gmail.com>
2023-07-05 18:23:19 +0200

    SocketConnect

--- app/src/components/Chat.jsx
+++ app/src/components/Chat.jsx
@@ -1,11 +1,32 @@
 import { useEffect } from "react";
 import socketIo from "socket.io-client";
+import ChatBar from "./ChatBar";
 
 const Chat = ({ res }) => {
   const socketConnect = socketIo.connect("http://localhost:8080", {
     extraHeaders: {
       "x-token": res.token,
     },
+  });
+
+  socketConnect.on("connect", () => {
+    console.log("Socket online");
+  });
+
+  socketConnect.on("disconnect", () => {
+    console.log("Socket offline");
+  });
+
+  socketConnect.on("send-message", () => {
+    console.log("Socket online");
+  });
+
+  socketConnect.on("active-users", () => {
+    console.log("Socket online");
+  });
+
+  socketConnect.on("private-message", () => {
+    console.log("Socket online");
   });
   //   socketConnect = socketIo({
   //     extraHeaders: {
@@ -18,7 +39,16 @@ const Chat = ({ res }) => {
 
   return (
     <div>
-      <h3>{res.user.name}</h3>
+      {/* <h3>{res.user.name}</h3> */}
+      <ChatBar />
+      <h3>Enviar mensaje</h3>
+      <input type="text" id="textId" autoComplete="off" />
+      <input
+        type="text"
+        id="text-message"
+        autoComplete="off"
+        placeholder="message"
+      />
     </div>
   );
 };
---
+++ app/src/components/ChatBar.jsx
@@ -0,0 +1,12 @@
+const ChatBar = () => {
+  return (
+    <div>
+      <h2>Open chat</h2>
+      <div>
+        <h4>Active User</h4>
+      </div>
+    </div>
+  );
+};
+
+export default ChatBar;
