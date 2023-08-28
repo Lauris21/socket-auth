3d6225ce321349a53fe2be9a6a33c9e7fd0dc71e
Lauris21 <laurasanchez0301@gmail.com>
2023-08-28 19:28:33 +0200

    message style

--- app/src/components/MessagesChat.jsx
+++ app/src/components/MessagesChat.jsx
@@ -3,13 +3,15 @@ const MessagesChat = ({ chat }) => {
   console.log(messages);
 
   return (
-    <div className="flex flex-col w-[95%] h-full">
+    <div className="flex flex-col w-[95%] h-[70%]">
       <div className="flex w-full justify-around items-center">
         <h3>{chat.userTwo.email}</h3>
         <img src={chat.userTwo.image} alt="image userTwo" className="w-10" />
       </div>
-      <div className="h-[90%]">
-        <div>{messages && messages.map((item) => item.text)}</div>
+      <div className="h-full">
+        <div className="flex flex-col">
+          {messages && messages.map((item) => <p>{item.text}</p>)}
+        </div>
       </div>
     </div>
   );
